223f162f3d659c617b6a92e955ca3c6a30ce6e87
Jonathan Ruiz <jontheruiz4@gmail.com>
2023-01-25 21:24:09 -0600

    gif hover to play

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -32,3 +26,14 @@
 		});
 
 })(jQuery);
+
+$(function(){
+	$("#myImg").hover(
+		function() {
+			$(this).attr("src", "images/Database_Showcase.gif");
+		},
+		function(){
+			$(this).attr("src", "images/Database_Showcase_Static.gif");
+		}
+	);
+});
